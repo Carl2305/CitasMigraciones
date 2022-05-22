91dd73ba6d8fbfff59ab618b1589dd0317bfe603
Carl2305 <mogollon.23carlos05@gmail.com>
2022-05-22 11:55:10 -0500

    Initial Commit

---
+++ CitasMigraciones/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CitasMigraciones
+//
+//  Created by Carlos Mogollon Espinoza on 5/22/22.
+//  Copyright © 2022 Carlos Alberto Mogollon Espinoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
