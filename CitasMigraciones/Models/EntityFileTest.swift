21abb7bf5b69eb6ca5b66df60f6ee84b82c22b8b
Carl2305 <mogollon.23carlos05@gmail.com>
2022-05-22 14:36:36 -0500

    Creación y estructura de Proyecto

---
+++ CitasMigraciones/Models/EntityFileTest.swift
@@ -0,0 +1,9 @@
+//
+//  EntityFileTest.swift
+//  CitasMigraciones
+//
+//  Created by Carlos Mogollon Espinoza on 5/22/22.
+//  Copyright © 2022 Carlos Alberto Mogollon Espinoza. All rights reserved.
+//
+
+import Foundation
