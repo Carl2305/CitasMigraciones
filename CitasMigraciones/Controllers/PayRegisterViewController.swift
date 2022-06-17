4a69193727b05f0b6da706207649f625f908c62d
Carl2305 <mogollon.23carlos05@gmail.com>
2022-06-17 18:42:25 -0500

    FUNCIONALIDADES - FINALIZADAS faltan validaciones en los formularios

--- CitasMigraciones/Controllers/PayRegisterViewController.swift
+++ CitasMigraciones/Controllers/PayRegisterViewController.swift
@@ -13,14 +13,11 @@ import SwiftyJSON
 class PayRegisterViewController: UIViewController {
 
     private let cupo:Cupo
-    @IBOutlet weak var dniTextField: UITextField!
+    
     @IBOutlet weak var numReciboTextField: UITextField!
     @IBOutlet weak var codVerifyTextField: UITextField!
     @IBOutlet weak var payDateDatePicker: UIDatePicker!
     @IBOutlet weak var registerCitaButton: UIButton!
-    @IBOutlet weak var validateDniButton: UIButton!
-    
-    private var isValidDni:Bool = false
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.isNavigationBarHidden=false
@@ -33,21 +30,7 @@ class PayRegisterViewController: UIViewController {
         title = "Registro Pago"
         registerCitaButton.borderRound()
         registerCitaButton.borderGreen()
-        
-        validateDniButton.borderRound()
-        validateDniButton.borderCyan()
-        
-        if(!self.isValidDni){
-            dniTextField.text = ""
-            validateDniButton.isEnabled = true
-            dniTextField.isEnabled = true
-            registerCitaButton.isEnabled = false
-            numReciboTextField.isEnabled = false
-            codVerifyTextField.isEnabled = false
-        }else{
-            self.cleanForm()
-        }
-        
+                
     }
 
     init(cupo:Cupo) {
@@ -59,55 +42,28 @@ class PayRegisterViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    @IBAction func validateDniButtonAction(_ sender: Any) {
-        let dni=dniTextField.text!
-        if(dni != ""){
-            self.validateDNI(dni: dni)
-        }else{
-            showAlert(title: "Error", message: "Ingrese un Documento de Identidad para Validar")
-        }
-    }
-    
     @IBAction func registerCitaButtonAction(_ sender: Any) {
         view.endEditing(true)
-        let dni = dniTextField.text!
+        //let dni = dniTextField.text!
         let numR = numReciboTextField.text!
         let codV = codVerifyTextField.text!
         let dateP = self.getDateForm(input: payDateDatePicker, format: "yyyy-MM-dd")
         
-        if(self.isValidDni){
-            if(dni != ""){
-                if(numR != ""){
-                    if(codV != ""){
-                        // aqui se debe llamar a la funcion que permite registrar
-                        // una cita mediante el servicio REST
-                        // si la cita se registra correctamente, debe redirigir al HomeViewController
-                        
-                        // esta es la funcion que se debe llamar -> rgisterRecibo(dni, numR, codV, date)
-                        
-                        // funcion momentanea
-                        showAlert(title: "Show DATA", message: "dni: \(dni), nRecibo: \(numR), cValida: \(codV), fRegistro: \(dateP)")
-                        
-                    }else{
-                        showAlert(title: "Error", message: "Ingrese un Código de Verificación")
-                    }
-                }else{
-                    showAlert(title: "Error", message: "Ingrese un Número de Recibo")
-                }
+        if(numR != ""){
+            if(codV != ""){
+                
+                self.rgisterRecibo(numR: numR, codV: codV, date: dateP)
+                
             }else{
-                validateDniButton.isEnabled = true
-                dniTextField.isEnabled = true
-                registerCitaButton.isEnabled = false
-                numReciboTextField.isEnabled = false
-                codVerifyTextField.isEnabled = false
-                showAlert(title: "Error", message: "Ingrese un Documento de Identidad")
+                showAlert(title: "Error", message: "Ingrese un Código de Verificación")
             }
         }else{
-            showAlert(title: "Error", message: "Antes debe validar su Documento de Identidad")
+            showAlert(title: "Error", message: "Ingrese un Número de Recibo")
         }
+        
     }
     
-    func rgisterRecibo(dni:String, numR:String, codV:String, date:String) {
+    func rgisterRecibo(numR:String, codV:String, date:String) {
         let URL_BASE:String = ConstantsCitasMigraciones.self().URL_BASE_API + "recibo"
         let params = [
             "codigoVoucher":numR,
@@ -121,7 +77,7 @@ class PayRegisterViewController: UIViewController {
                 let json=JSON(val)
                 let dataResponse = ApiResponseCreated(mensaje: json["mensaje"].stringValue, status: json["status"].stringValue)
                 if(dataResponse.status=="OK"){
-                    self.consultReciboRegister(dni: dni, numR: numR)
+                    self.consultReciboRegister(numR: numR)
                 }else{
                     self.cleanForm()
                     self.showAlert(title: "Error", message: dataResponse.mensaje)
@@ -134,7 +90,7 @@ class PayRegisterViewController: UIViewController {
         }
     }
     
-    func consultReciboRegister(dni:String, numR:String){
+    func consultReciboRegister(numR:String){
         let URL_BASE:String = ConstantsCitasMigraciones.self().URL_BASE_API + "recibo/cv/\(numR)"
         Alamofire.request(URL_BASE, method: .get, encoding: JSONEncoding.default).responseJSON{ (response) in
             switch response.result{
@@ -142,11 +98,8 @@ class PayRegisterViewController: UIViewController {
             case .success(let val):
                 let json=JSON(val)
                 if(json["status"].stringValue == "OK"){
-                    let recibo:Recibo = Recibo(id_recibo: json["data"]["id_recibo"].intValue, codigoVoucher: json["data"]["codigoVoucher"].stringValue,
-                                               codigoVerificacion: json["data"]["codigoVerificacion"].stringValue, fechaPago: json["data"]["fechaPago"].stringValue)
-                    let client:Cliente = Cliente(nombre: "", apePaterno: "", apeMaterno: "", correo: "", password: "", direccion: "", fechaNac: "", dni: dni)
-                    // aqui se debe llamar a la funcion que registra la cita en la DB
-                    // la funcion que se debe llamar  es -> registerCita()
+                    
+                    self.registerCita(cup: self.cupo, idRecb: json["data"]["id_recibo"].intValue, dni: UsersDefaultsCitasMigraciones.self().getDni())
                 }
             case .failure(let error):
                 print(error.localizedDescription)
@@ -154,21 +107,31 @@ class PayRegisterViewController: UIViewController {
         }
     }
     
-    func registerCita(cup:Cupo, recb:Recibo, cli:Cliente){
+    func registerCita(cup:Cupo, idRecb:Int, dni:String){
         let URL_BASE:String = ConstantsCitasMigraciones.self().URL_BASE_API + "cita"
-        let params = [
-            "recibo":recb,
-            "cupo":cup,
-            "cliente":cli
-            ] as [String : Any] 
+        let paramCli:[String:Any] = [
+            "dni":"\(dni)"
+        ]
+        let paramCup:[String:Any] = [
+            "idCupo":"\(cup.idCupo)"
+        ]
+        let paramRicb:[String:Any] = [
+            "id_recibo":"\(idRecb)"
+        ]
+        let params:[String:Any] = [
+            "recibo":paramRicb,
+            "cupo":paramCup,
+            "cliente":paramCli
+        ]
         
         Alamofire.request(URL_BASE, method: .post, parameters: params, encoding: JSONEncoding.default).responseJSON{ (response) in
             switch response.result{
                 
             case .success(let val):
                 let json=JSON(val)
+                print("response - > ", json)
                 if(json["status"].stringValue == "OK"){
-                    // sedebe redirigir al home
+                    self.updateStatusCupo(cp: cup)
                 }else{
                     self.cleanForm()
                     self.showAlert(title: "Error", message: "No se pudo registrar la cita.")
@@ -181,28 +144,28 @@ class PayRegisterViewController: UIViewController {
         }
     }
     
-    func validateDNI(dni:String){
-        let URL_BASE:String = ConstantsCitasMigraciones.self().URL_BASE_API + "auth/validarDNI/\(dni)"
-        Alamofire.request(URL_BASE, method: .get, encoding: JSONEncoding.default).responseJSON{ (response) in
+    func updateStatusCupo(cp:Cupo){
+        let URL_BASE:String = ConstantsCitasMigraciones.self().URL_BASE_API + "cupo"
+        let paramSede:[String:Any] = [
+            "idSede":"\(cp.sede.idSede)"
+        ]
+        let params:[String:Any] = [
+            "idCupo":cp.idCupo,
+            "sede":paramSede,
+            "fechaCupo":cp.fechaCupo
+        ]
+        
+        Alamofire.request(URL_BASE, method: .put, parameters: params, encoding: JSONEncoding.default).responseJSON{ (response) in
             switch response.result{
                 
             case .success(let val):
                 let json=JSON(val)
-                let dataResponse = ApiResponseCreated(mensaje: json["mensaje"].stringValue, status: json["status"].stringValue)
-                if(dataResponse.status=="OK"){
-                    self.showAlertValidateSuccessOrError(message: "¿Quieres Registrar una Cita a este Nombre: \(dataResponse.mensaje)?")
-                }else{
-                    self.showAlert(title: "Error Validación", message: "No se pudo validar el DNI.")
-                    self.validateDniButton.isEnabled = true
-                    self.dniTextField.isEnabled = true
-                    self.isValidDni = false
-                    self.registerCitaButton.isEnabled = false
-                    self.numReciboTextField.isEnabled = false
-                    self.codVerifyTextField.isEnabled = false
+                print("response - > ", json)
+                if(json["status"].stringValue == "OK"){
+                    self.showAlertRedirectHome(message: "La cita se registro exitosamente!")
                 }
             case .failure(let error):
                 print(error.localizedDescription)
-               self.showAlert(title: "Error", message: "No se pudo validar el DNI, error en el servicio")
             }
         }
     }
@@ -221,40 +184,19 @@ class PayRegisterViewController: UIViewController {
         self.present(alertController, animated: true, completion: nil)
     }
     
-    func showAlertValidateSuccessOrError(message:String){
-        let alert=UIAlertController(title: "Validación Exitosa", message: message, preferredStyle: .alert)
-        
-        let okButtonAction=UIAlertAction(title: "Si, es Correcto", style: .default){
+    func showAlertRedirectHome(message:String){
+        let alertController=UIAlertController(title: "Cita Registrada", message: message, preferredStyle: .alert)
+        let okButtonAction=UIAlertAction(title: "OK", style: .default){
             (action:UIAlertAction) in
-            self.validateDniButton.isEnabled = false
-            self.dniTextField.isEnabled = false
-            self.isValidDni = true
-            self.registerCitaButton.isEnabled = true
-            self.numReciboTextField.isEnabled = true
-            self.codVerifyTextField.isEnabled = true
-        }
-        
-        let cancelButtonAction=UIAlertAction(title: "No", style: .default){
-            (action: UIAlertAction) in
             self.cleanForm()
+            self.navigationController?.pushViewController(HomeViewController(), animated: true)
         }
-        
-        alert.addAction(okButtonAction)
-        alert.addAction(cancelButtonAction)
-        
-        present(alert, animated: true, completion: nil)
-        
+        alertController.addAction(okButtonAction)
+        self.present(alertController, animated: true, completion: nil)
     }
     
     func cleanForm(){
-        dniTextField.text = ""
         numReciboTextField.text = ""
         codVerifyTextField.text = ""
-        self.isValidDni = false
-        validateDniButton.isEnabled = true
-        dniTextField.isEnabled = true
-        registerCitaButton.isEnabled = false
-        numReciboTextField.isEnabled = false
-        codVerifyTextField.isEnabled = false
     }
 }
